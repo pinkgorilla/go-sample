9d6e3a3eddae16c4907f5c4b5e0935fd6d4364d4
Tris Setiawan <payfazz131@Payfazzs-MacBook-Pro-33.local>
2020-06-09 13:21:11 +0700

    restructure builder pattern

--- patterns/creational/builder/builder_test.go
+++ patterns/creational/builder/builder_test.go
@@ -36,6 +36,10 @@ import "testing"
 // - When using a motorbike builder, the VehicleProduct with two wheels, two seats, and a structure defined as Motorbike must be returned
 // - A VehicleProduct built by any BuildProcess builder must be open to modifications
 
+// This following interface defines the steps that are necessary to build a vehicle.
+// Every builder must implement this interface if they are to be used by the manufacturing.
+// On every Set step, we return the same build process, so we can chain various steps together in the same statement, as we'll see later.
+// Finally, we'll need a GetVehicle method to retrieve the Vehicle instance from the builder:
 type BuildProcess interface {
 	SetWheels() BuildProcess
 	SetSeats() BuildProcess
@@ -43,11 +47,9 @@ type BuildProcess interface {
 	GetVehicle() VehicleProduct
 }
 
-// This preceding interface defines the steps that are necessary to build a vehicle.
-// Every builder must implement this interface if they are to be used by the manufacturing.
-// On every Set step, we return the same build process, so we can chain various steps together in the same statement, as we'll see later.
-// Finally, we'll need a GetVehicle method to retrieve the Vehicle instance from the builder:
-
+// The ManufacturingDirector director variable is the one in charge of accepting the builders.
+// It has a Construct method that will use the builder that is stored in Manufacturing, and will reproduce the required steps.
+// The SetBuilder method will allow us to change the builder that is being used in the Manufacturing director:
 type ManufacturingDirector struct {
 	builder BuildProcess
 }
@@ -60,18 +62,17 @@ func (f *ManufacturingDirector) Construct() {
 	f.builder.SetSeats().SetStructure().SetWheels()
 }
 
-// The ManufacturingDirector director variable is the one in charge of accepting the builders.
-// It has a Construct method that will use the builder that is stored in Manufacturing, and will reproduce the required steps.
-// The SetBuilder method will allow us to change the builder that is being used in the Manufacturing director:
-
+// The product is the final object that we want to retrieve while using the manufacturing.
+// In this case, a vehicle is composed of wheels, seats, and a structure:
 type VehicleProduct struct {
 	Wheels    int
 	Seats     int
 	Structure string
 }
 
-//  The product is the final object that we want to retrieve while using the manufacturing.
-// In this case, a vehicle is composed of wheels, seats, and a structure:
+// The first Builder is the Car builder.
+// It must implement every method defined in the BuildProcess interface.
+// This is where we'll set the information for this particular builder:
 type CarBuilder struct {
 	v VehicleProduct
 }
@@ -95,9 +96,10 @@ func (c *CarBuilder) GetVehicle() VehicleProduct {
 	return c.v
 }
 
-// The first Builder is the Car builder.
-// It must implement every method defined in the BuildProcess interface.
-// This is where we'll set the information for this particular builder:
+// The Motorbike structure must be the same as the Car structure, as they are all Builder implementations,
+// but keep in mind that the process of building each can be very different. With this declaration of objects,
+// we can create the following tests:
+
 type BikeBuilder struct {
 	v VehicleProduct
 }
@@ -121,24 +123,19 @@ func (b *BikeBuilder) GetVehicle() VehicleProduct {
 	return b.v
 }
 
-// The Motorbike structure must be the same as the Car structure, as they are all Builder implementations,
-// but keep in mind that the process of building each can be very different. With this declaration of objects,
-// we can create the following tests:
-
 func TestBuilderPattern(t *testing.T) {
+	// We will start with the Manufacturing director and the Car Builder to fulfill the first two acceptance criteria.
+	// In the preceding code, we are creating our Manufacturing director that will be in charge of the creation of every vehicle during the test.
 	manufacturingComplex := ManufacturingDirector{}
 
+	// After creating the Manufacturing director, we created a CarBuilder that we then passed to manufacturing by using the SetBuilder method.
+	// Once the Manufacturing director knows what it has to construct now, we can call the Construct method to create the VehicleProduct using CarBuilder.
+	// Finally, once we have all the pieces for our car, we call the GetVehicle method on CarBuilder to retrieve a Car instance:
 	carBuilder := &CarBuilder{}
 	manufacturingComplex.SetBuilder(carBuilder)
 	manufacturingComplex.Construct()
 
 	car := carBuilder.GetVehicle()
-
-	// We will start with the Manufacturing director and the Car Builder to fulfill the first two acceptance criteria.
-	// In the preceding code, we are creating our Manufacturing director that will be in charge of the creation of every vehicle during the test.
-	// After creating the Manufacturing director, we created a CarBuilder that we then passed to manufacturing by using the SetBuilder method.
-	// Once the Manufacturing director knows what it has to construct now, we can call the Construct method to create the VehicleProduct using CarBuilder.
-	// Finally, once we have all the pieces for our car, we call the GetVehicle method on CarBuilder to retrieve a Car instance:
 
 	if car.Wheels != 4 {
 		t.Errorf("Wheels on a car must be 4 and they were %d\n", car.Wheels)
@@ -154,6 +151,10 @@ func TestBuilderPattern(t *testing.T) {
 	// Now we will create tests for a Motorbike builder that covers the third and fourth acceptance criteria:
 	bikeBuilder := &BikeBuilder{}
 
+	// The following code is a continuation of the car tests.
+	// As you can see, we reuse the previously created manufacturing to create the bike now by passing the Motorbike builder to it.
+	// Then we hit the construct button again to create the necessary parts,
+	// and call the builder GetVehicle method to retrieve the motorbike instance.
 	manufacturingComplex.SetBuilder(bikeBuilder)
 	manufacturingComplex.Construct()
 
@@ -167,10 +168,6 @@ func TestBuilderPattern(t *testing.T) {
 	if motorbike.Structure != "Motorbike" {
 		t.Errorf("Structure on a motorbike must be 'Motorbike' and was %s\n", motorbike.Structure)
 	}
-	// The preceding code is a continuation of the car tests.
-	// As you can see, we reuse the previously created manufacturing to create the bike now by passing the Motorbike builder to it.
-	// Then we hit the construct button again to create the necessary parts,
-	// and call the builder GetVehicle method to retrieve the motorbike instance.
 }
 
 // ## Wrapping up the Builder design pattern
@@ -181,3 +178,5 @@ func TestBuilderPattern(t *testing.T) {
 // However, try to avoid the Builder pattern when you are not completely sure that the algorithm is going to be more or less stable
 // because any small change in this interface will affect all your builders and
 // it could be awkward if you add a new method that some of your builders need and others Builders do not.
+
+// Excerpt From: “Go: Design Patterns for Real-World Projects.” Apple Books.
